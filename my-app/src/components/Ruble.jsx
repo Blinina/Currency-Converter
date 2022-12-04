118e6b1f7889cf3150e4c4c41cb634227e84ea94
Blinina <anastasia.paancake@gmail.com>
2022-12-04 13:19:33 +0300

    nav and ruble css done

--- my-app/src/components/Ruble.jsx
+++ my-app/src/components/Ruble.jsx
@@ -1,34 +1,42 @@
-import {React,  useEffect } from "react"
+import { React, useEffect } from "react"
 import { useDispatch, useSelector } from 'react-redux';
-import {getData} from './sliceCurrencies'
+import { getData } from './sliceCurrencies'
+import red from "../assets/images/red.png";
+import blue from "../assets/images/blue.png";
+import info from "../assets/images/info.png";
+export default function Ruble() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getData())
+    }, [dispatch])
+
+    const data = useSelector((state) => state.currencies.currencies);
+    const currencies = Object.entries(data).slice(0, 3);
+    const images = [red, blue, red];
 
 
-export default function Ruble(){
-const dispatch = useDispatch();
-
-useEffect(()=>{
-dispatch(getData())
-}, [dispatch])
-
-const data = useSelector((state)=>state.currencies.currencies);
-const currencies = Object.entries(data).slice(0, 3);
-console.log(currencies)
-
-
-    return(
-        <div>
-            <h2>Курс рубля</h2>
+    return (
+        <div className="rubles">
+            <h2 className="title">Курс рубля</h2>
             <div className="cards">
-                {currencies.map(([,b])=><div key={b.ID} className="card">
+
+                {currencies.map(([, b], index) => <div key={b.ID} className="card">
+                    <img src={images[index]} alt={`${images[index]} картинка`} />
                     <p className="char">{b.CharCode}</p>
-                    <p className="name">{b.Name}</p>
-                    <p className="nominal">Номинал: {b.Nominal}</p>
-                    <p className="value">Курс: {b.Value}</p>
+                    <div className="card-body">
+                        <p className="name">{b.Name}</p>
+                        <ul>
+                        <li className="nominal">Номинал: {b.Nominal}</li>
+                        <li className="value">Курс: {b.Value}</li>
+                        </ul>
                     </div>
-                    )}
+                </div>
+                )}
             </div>
-            <div>
-                <div><p>Телефон: <a href="tel:+78008889028">8 (800) 888-90-28</a></p><p>email: <a href="mailto:info@example.ru">info@example.ru</a></p></div>
+            <div className="footer">
+                <img src={info} alt="инфо-лого" className="info"/>
+                <div className="text"><p>Телефон: <a href="tel:+78008889028">8 (800) 888-90-28</a>, email: <a href="mailto:info@example.ru">info@example.ru</a></p></div>
             </div>
         </div>
     )
